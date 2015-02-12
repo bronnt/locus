f71866442ad491d103348a7ce746ef5032018aae
bronnt <bthurber@Brian-T.local>
2015-02-11 23:29:44 -0700

    stuff

--- themes/locus-realty/functions.php
+++ themes/locus-realty/functions.php
@@ -146,6 +146,10 @@ add_action( 'widgets_init', 'locus_realty_widgets_init' );
  */
 function locus_realty_scripts() {
 
+	//wp_enqueue_style( 'locus-realty-screen', get_template_directory_uri() . '/css/screen.css', array(), '20150207', 'all' );
+
+	wp_enqueue_style( 'locus-realty-lightbox', get_template_directory_uri() . '/css/lightbox.css', array(), '20150207', 'all' );
+
 	wp_enqueue_style( 'locus-realty-bootstrap', get_template_directory_uri() . '/css/bootstrap.css', array(), '20150207', 'all' );
 
 	wp_enqueue_style( 'locus-realty-flexslider-style', '/wp-content/plugins/utah-banner-widget/flexslider.css', array(), '20150207', 'all' ); 
@@ -162,7 +166,8 @@ function locus_realty_scripts() {
 
 	wp_enqueue_script( 'locus-realty-global',  get_template_directory_uri() . '/js/global.js', array('jquery'), '20150207', true );
 
-	wp_enqueue_script( 'locus-realty-global',  get_template_directory_uri() . '/js/retina.min.js', array('jquery'), '20150207', true );
+	wp_enqueue_script( 'locus-realty-retina',  get_template_directory_uri() . '/js/retina.min.js', array('jquery'), '20150207', true );
+	wp_enqueue_script( 'locus-realty-lightbox',  get_template_directory_uri() . '/js/lightbox.min.js', array('jquery'), '20150207', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
